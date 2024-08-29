e96a4f639acdaa3fcadc05acd644e13fffe15fd5
Dragon <31087863+dragon070508@users.noreply.github.com>
2024-08-29 12:07:07 -0700

    Added inputs and intro

---
+++ AffirmationGen - 1.py
@@ -0,0 +1,10 @@
+print("Welcome to your Daily Affirmations (or Insults, jk) Generator!")
+
+print("What's your name?")
+name = input()
+
+print("What day of the week is it today?")
+day = input()
+
+print("What's one of your favorite things?")
+favorite_thing = input()
