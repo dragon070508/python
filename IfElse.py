9d73606e3da30f2099cb1ff1608a23b817655865
Dragon <31087863+dragon070508@users.noreply.github.com>
2024-08-29 11:01:18 -0700

    Learning about if else

---
+++ IfElse.py
@@ -0,0 +1,6 @@
+myName = input("What's your name?: ")
+if myName == "Spongebob" or "spongebob":
+ print("Welcome Dude!")
+ print("You're just the spongiest dude I've ever seen")
+else:
+ print("Who on earth are you?!")
