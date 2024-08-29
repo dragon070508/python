8e98dbc2ed47fbab3687e421d96924e1ad2af3a1
Dragon <31087863+dragon070508@users.noreply.github.com>
2024-08-29 08:36:48 -0700

    concatenating test

---
+++ main (3).py
@@ -0,0 +1,8 @@
+myName = input("What's your name? ")
+myLunch = input("What are you having for lunch? ")
+print(myName, "is going to be chowing down on", myLunch, "very soon!")
+
+number = input("Give me a number: ")
+group = input("Give me a collective noun for a group of things: ")
+thing = input("Give me the name of a weird or wacky thing: ")
+print("No I don't think that", number, "is a", group, "of", thing,". That's just odd.")
