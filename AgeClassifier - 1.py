d8c0fc663a1d18547fe888b1c31b491e6c428be2
Dragon <31087863+dragon070508@users.noreply.github.com>
2024-08-29 20:26:42 -0700

    Started frame of code

---
+++ AgeClassifier - 1.py
@@ -0,0 +1,8 @@
+print("Welcome to the Generation Identifier!")
+
+print("Please enter the year you were born:")
+birth_year = int(input())
+
+if birth_year >= 1925 and birth_year <= 1946:
+    print("🎩 You belong to the Traditionalists generation (1925-1946). A time of great values and strength!")
+elif birth_year >= 1947 and birth_year <= 1964:
