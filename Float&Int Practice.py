6dacc8b4b6eb2317d7a117aecf43c39f74aae0f1
Dragon <31087863+dragon070508@users.noreply.github.com>
2024-08-29 20:20:33 -0700

    Practicing by debugging code with errors

---
+++ Float&Int Practice.py
@@ -0,0 +1,19 @@
+myPi = float(input("What is Pi to 3dp? "))
+if myPi == 3.142:
+  print("Exactly!")
+else:
+  print("Try again 😭")
+
+myPi = float(input("What is Pi to 3dp?"))
+if myPi == 3.142 :
+  print("Exactly!")
+else:
+  print("Try again 😭")
+
+score = int(input("What was your score on the test?"))
+if score >= 80:
+  print("Not too shabby")
+elif score > 70:
+  print("Acceptable.")
+else:
+  print("Dude, you need to study more!")
