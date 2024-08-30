e4d4d30fcbc2c664d7502f46deb3f80e6ef8ffa3
Dragon <31087863+dragon070508@users.noreply.github.com>
2024-08-29 20:56:08 -0700

    Calc base

---
+++ Calc - 1.py
@@ -0,0 +1,10 @@
+print("Welcome to the Simple Calculator!")
+
+print("Enter the first number:")
+num1 = float(input())
+
+print("Enter the operation (+, -, *, /):")
+operation = input()
+
+print("Enter the second number:")
+num2 = float(input())
